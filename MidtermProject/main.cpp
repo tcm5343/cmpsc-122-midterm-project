50b6257065ee7e6ea139b78eea4d5146dfc34145
tcm5343 <floyd68@protonmail.com>
2020-04-20 13:10:44 -0400

    init repo

---
+++ MidtermProject/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: miller
+ *
+ * Created on April 20, 2020, 1:00 PM
+ */
+
+#include <iostream>
+
+/*
+ * 
+ */
+int main() {
+    
+    std::cout << "hi";
+    
+    return 0;
+}
+
